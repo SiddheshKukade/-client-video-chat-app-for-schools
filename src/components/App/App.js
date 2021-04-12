148afe608113a7e6b7a80de0bede617955ddba02
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2021-04-12 18:56:22 +0530

    login front-end

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-
+import Login from '../Login/Login'
 const App = () => {
   return (
-    <h1 className="text-indigo-200  bg-red-500   ">
-      Jai Aai Jai Baba
-      <hr />
-      <hr />
-      <hr />
-      Om GanaPatay Namah <hr /> Om namah Shivay
-    </h1>
+    <Login/>
+    // <h1 className="text-indigo-200  bg-red-500   ">
+    //   Jai Aai Jai Baba
+    //   <hr />
+    //   <hr />
+    //   <hr />
+    //   Om GanaPatay Namah <hr /> Om namah Shivay
+    // </h1>
   );
 };
 
