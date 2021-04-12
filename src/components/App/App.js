5c7da1f5c4156506f1ae287cf6b9154ac052daff
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2021-04-12 10:09:13 +0530

    tailwind setup

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="text-indigo-200  bg-red-500   ">
+      Jai Aai Jai Baba
+      <hr />
+      <hr />
+      <hr />
+      Om GanaPatay Namah <hr /> Om namah Shivay
+    </h1>
+  );
+};
+
+export default App;
