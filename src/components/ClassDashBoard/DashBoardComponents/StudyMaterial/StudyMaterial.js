ae574e61b167f2e8331fe8460758241ee0e78c64
SiddheshKukade <littlelord124@gmail.com>
2021-06-13 11:31:31 +0530

    implement studyt matetial downlaod for student logic

--- src/components/ClassDashBoard/DashBoardComponents/StudyMaterial/StudyMaterial.js
+++ src/components/ClassDashBoard/DashBoardComponents/StudyMaterial/StudyMaterial.js
@@ -3,14 +3,27 @@ import StudyMaterialPost from "./StudyMaterialPost/StudyMaterialPost";
 import { Helmet } from "react-helmet";
 import AddStudyMaterialPanel from "../../AddStudyMaterialPanel/AddStudyMaterialPanel";
 import styles from "./StudyMaterial.module.css";
-function StudyMaterial({ studyMaterialList }) {
+function StudyMaterial({ studyMaterialList, studyMaterialPosts }) {
   return (
     <div className={styles.mainContainer}>
       <Helmet>
         <title>Study Material</title>
       </Helmet>
       {/* <TitleComponent title={title} /> */}
-      <StudyMaterialPost />
+      {studyMaterialPosts.map((post) => (
+        <StudyMaterialPost
+          postedAt={post.postedAt}
+          fromSchoolRef={post.fromSchoolRef}
+          fromTeacherMail={post.fromTeacherMail}
+          name={post.name}
+          file={post.file}
+          subject={post.subject}
+          standard={post.standard}
+          createdAt={post.createdAt}
+          updatedAt={post.updatedAt}
+        />
+      ))}
+
       <AddStudyMaterialPanel />
     </div>
   );
--- src/components/ClassDashBoard/DashBoardComponents/StudyMaterial/StudyMaterialPost/StudyMaterialPost.js
+++ src/components/ClassDashBoard/DashBoardComponents/StudyMaterial/StudyMaterialPost/StudyMaterialPost.js
@@ -1,6 +1,48 @@
 import React from "react";
 import "./StudyMaterialPost.css";
-function StudyMaterialPost() {
+import Moment from "react-moment";
+
+function StudyMaterialPost({
+  postedAt,
+  fromSchoolRef,
+  fromTeacherMail,
+  name,
+  file,
+  subject,
+  standard,
+  createdAt,
+  updatedAt,
+}) {
+  const downloadDoc = () => {
+    console.log("cliucked");
+    // fetch('https://source.unsplash.com/user/erondu/1600x900', {
+    fetch("https://jsonplaceholder.typicode.com/todos/", {
+      crossDomain: true,
+      method: "GET",
+      headers: {
+        // 'Content-Type': 'application/jpeg'
+        "Content-Type": "application/text",
+      },
+    })
+      .then((response) => response.blob())
+      .then((blob) => {
+        // Create blob link to download
+        const url = window.URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement("a");
+        link.href = url;
+        // link.setAttribute('download', `sidd.jpeg`);
+        link.setAttribute("download", `sidd`);
+
+        // Append to html link element page
+        document.body.appendChild(link);
+
+        // Start download
+        link.click();
+
+        // Clean up and remove the link
+        link.parentNode.removeChild(link);
+      });
+  };
   return (
     <div className="post__wrapper">
       <div className="upper__wrapper">
@@ -22,24 +64,32 @@ function StudyMaterialPost() {
         </div>
         <div className="second__wrapper">
           <div className="post__title">
-            Teacher has posted Chapter Two Notes
+            {/* Teacher has posted Chapter Two Notes */}
+            {name}
           </div>
-          <div className="post__date">23.11.2021</div>
+          <div className="post__date">
+            {/* 23.11.2021 */} Posted
+            <Moment fromNow interval={100}>
+              {postedAt.toISOString()}
+            </Moment>{" "}
+          </div>
         </div>
       </div>
       <div className="third__wrapper" title="Download Material">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-            clip-rule="evenodd"
-          />
-        </svg>
+        <a onClick={downloadDoc} download>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </a>
       </div>
     </div>
   );
